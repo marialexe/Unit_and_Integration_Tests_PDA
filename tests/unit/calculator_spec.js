e39f59c0beded2cc755268a5524851a0e451087d
Marilena Alexe <marilena.alexe@gmail.com>
2017-08-09 22:06:37 +0100

    Start point for PDA Unit and Integration Testing Task

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,16 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  var calculator;
+
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  });
+
+});
