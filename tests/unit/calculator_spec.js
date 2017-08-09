a4a6fa16b412ae26c351588f4c880f05cb202404
Marilena Alexe <marilena.alexe@gmail.com>
2017-08-09 22:12:20 +0100

    Unit testing - add < can click a one digit number> test

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -13,4 +13,8 @@ describe('calculator', function () {
     assert.equal(true, true)
   });
 
+  //due to the design of the calculator can't do tests for decimal numbers as there is no "." button. This test can't be reproduced in manual testing for this calculator.
+  //due to the design of the calculator can't do tests with negative numbers (as inputs) as there is no functionality to add "-" in front of numbers. This test can't be reproduced in manual testing for this calculator.
+  
+
 });
