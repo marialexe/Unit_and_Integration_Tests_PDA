d83734e43bb20c5d8de01149b4fe5ae38a6763a9
Marilena Alexe <marilena.alexe@gmail.com>
2017-08-10 08:07:41 +0100

    Integration testing - add <should correctly update the running total when clicking the divide operator 2nd> test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -137,4 +137,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('3');
   })
 
+  it('should correctly update the running total when clicking the divide operator test2', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('26');
+  })
+
 });
