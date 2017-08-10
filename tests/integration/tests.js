9ff546b90a3aa1eb262cc45ff43017c9d6035be6
Marilena Alexe <marilena.alexe@gmail.com>
2017-08-10 07:43:15 +0100

    Integration testing - add <should correctly update the running total when clicking the add operator 1st> test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -68,4 +68,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('7890')
   })
 
+  it('should correctly update the running total when clicking the add operator test1', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.buttonText('6')).click();
+    element(by.buttonText('+')).click();
+    element(by.buttonText('4')).click();
+    element(by.buttonText('=')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10');
+  })
+
 });
